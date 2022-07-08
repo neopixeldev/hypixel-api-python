d9e1dc8ec299837fcc50362d71b2f54f8e87dcba
iTrojang <95707186+iTrojang@users.noreply.github.com>
2022-07-08 22:20:56 +0100

    Create pynapple.py

---
+++ Pynapple/pynapple.py
@@ -0,0 +1,47 @@
+import requests
+
+class Pynapple(object):
+    def __init__(self,key,name):
+        self.key = key
+        self.name = name
+
+    @staticmethod
+    def getName():
+        return
+
+    @staticmethod
+    def getUuid():
+        return
+
+    @staticmethod
+    def getRank():
+        return
+
+    @staticmethod
+    def LastLogin():
+        return
+
+    @staticmethod
+    def FirstLogin():
+        return
+
+    @staticmethod
+    def LastLogout():
+        return
+
+    class Bedwars(object):
+        def init(self):
+            return
+
+    class Skyblock(object):
+        def init(self):
+            return
+
+    class Duels(object):
+        def init(self):
+            return
+
+    class Skywars(object):
+        def init(self):
+            return
+
