c35c72a25057b80ba2fe3ad314bd890b17dfc262
iTrojang <95707186+iTrojang@users.noreply.github.com>
2022-07-08 23:08:41 +0100

    Create pynapple.py

---
+++ Pynapple/pynapple.py
@@ -0,0 +1,60 @@
+import requests
+import datetime
+
+class Pynapple(object):
+    def __init__(self,key):
+        self.key = key
+
+
+    def fetch(self,url):
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def getName(self,name):
+        uuid = self.fetch(url=f'https://api.mojang.com/users/profiles/minecraft/{name}')['id']
+        hyapi = self.fetch(f'https://api.hypixel.net/player?key={self.key}&uuid={uuid}')
+        return hyapi['player']['name']
+
+
+    def getUuid(self,name):
+        uuid = Pynapple.fetch(f'https://api.mojang.com/users/profiles/minecraft/{name}')['id']
+        return uuid
+
+    def getRank(self,name):
+        uuid = self.fetch(f'https://api.mojang.com/users/profiles/minecraft/{name}')['id']
+        hyapi = self.fetch(f'https://api.hypixel.net/player?key={self.key}&uuid={uuid}')
+        return hyapi['player']['newPackageRank']
+
+    def LastLogin(self,name):
+        uuid = self.fetch(f'https://api.mojang.com/users/profiles/minecraft/{name}')['id']
+        hyapi = self.fetch(f'https://api.hypixel.net/player?key={self.key}&uuid={uuid}')
+        return datetime.datetime.fromtimestamp(hyapi['player']['lastLogin'] / 1000)
+
+    def FirstLogin(self,name):
+        uuid = self.fetch(f'https://api.mojang.com/users/profiles/minecraft/{name}')['id']
+        hyapi = self.fetch(f'https://api.hypixel.net/player?key={self.key}&uuid={uuid}')
+        time = hyapi['player']['firstLogin']
+        return datetime.datetime.fromtimestamp(time / 1000)
+
+    def LastLogout(self,name):
+        uuid = self.fetch(f'https://api.mojang.com/users/profiles/minecraft/{name}')['id']
+        hyapi = self.fetch(f'https://api.hypixel.net/player?key={self.key}&uuid={uuid}')
+        time = hyapi['player']['lastLogout']
+        return datetime.datetime.fromtimestamp(time / 1000)
+
+    class Bedwars(object):
+        pass
+
+    class Skyblock(object):
+        def init(self):
+            return
+
+    class Duels(object):
+        def init(self):
+            return
+
+    class Skywars(object):
+        def init(self):
+            return
+
