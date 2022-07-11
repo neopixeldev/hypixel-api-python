2d6987ffe44f6378dda4590f05b079855f31041d
PingIsFun <pingisfun@protonmail.com>
2022-07-11 10:42:48 +0200

    Refactor fetch and get
    Refactor fetch and get and make them use paramethers and headers instead
    of an url

--- src/hypxiel_api_python/utils/cache.py
+++ src/hypxiel_api_python/utils/cache.py
@@ -1,12 +1,24 @@
 from datetime import datetime
 
+
 # This is currently only set up to purge values that can't be cached for long, e.g. a hypixel player
 # Support will need to be added for name caching
 
 
-async def purge_cache(self):
+async def purge_cache(hypixel_api, /, *, force=False) -> None:
+    """
+    It purges the cache of old data
+
+    :param hypixel_api: The HypixelAPI object
+    :param force: If set to True, it will purge the entire cache. If set to False, it will purge the cache of old data, defaults to False (optional)
+    :return: None
+    """
     """Purges the cache of old data"""
-    requests = self.request_cache
+    requests = hypixel_api.request_cache
+    if force:
+        hypixel_api.request_cache = {}
+        return
     for key in requests:
         if requests[key]["hypixel-api-python"]["timestamp"] + 60 < int(datetime.now().timestamp()):
             del requests[key]
+    return
--- src/hypxiel_api_python/utils/requests.py
+++ src/hypxiel_api_python/utils/requests.py
@@ -1,48 +1,63 @@
-from src.hypxiel_api_python.utils.cache import purge_cache
-from urllib.parse import urlparse, parse_qs
-from aiohttp import ClientSession
 from datetime import datetime
+from aiohttp import ClientSession
+from src.hypxiel_api_python.utils.cache import purge_cache
+from src.hypxiel_api_python.hypixel import HypixelAPI
 
 
-async def get(self, endpoint: str, param: tuple, nocache: bool = None):
+async def get(hypixel_api: HypixelAPI, endpoint: str, params: dict[str, str], nocache: bool = None) -> dict:
     """Determine whether it should use a cached object or fetch a new one."""
     """`nocache` is a way for the user to bypass the cache"""
-    await purge_cache(self)
-    api = self.hypixel_api
-    cached_response = self.hypixel_api.request_cache[param][f"{endpoint.replace('/', '')}.{param[1]}"]
-    if not cached_response:
-        return await fetch(self=self, url=f"{api.path}/{endpoint}?key={api.key}&{param[0]}={param[1]}")
+    if not endpoint.startswith("/"):
+        endpoint = f"/{endpoint}"
 
-    elif cached_response["hypixel-api-python"]["timestamp"] + 60 > int(datetime.now().timestamp()) or nocache:
-        return await fetch(self=self, url=f"{api.path}/{endpoint}?key={api.key}&{param[0]}={param[1]}")
+    await purge_cache(hypixel_api)
+    auth_header = {"API-Key": hypixel_api.key}
+    try:
+        cached_response = hypixel_api.request_cache[f"{endpoint}.{params}"]
+        # print(cached_response)
+    except KeyError:
+        cached_response = None
+    # if not cached_response:
+    #     return await fetch(self, endpoint, params, auth_header)
+    if not cached_response or cached_response["hypixel-api-python"]["timestamp"] + 60 > int(datetime.now().timestamp()) or nocache:
+        return await fetch_hypxiel_api(hypixel_api, endpoint, params, auth_header)
 
     else:
         return cached_response["response"]
 
 
-async def fetch(self, url: str):
-    """Fetches data from the Hypixel API"""
-    # stored as variables for readability
-    parsed = urlparse(url)
-    params = parse_qs(parsed.query)
-    param_name = list(params.keys())[1]
-    param_value = params[param_name][0]
+async def fetch_hypxiel_api(hypixel_api: HypixelAPI, endpoint: str, params: dict[str, str], headers: dict[str, str]) -> dict:
+    """
+    Fetches data from the Hypixel API and returns it as a dictionary
 
-    api = self.hypixel_api
-    async with ClientSession() as session:
-        async with session.get(url) as request:
+    :param hypixel_api: The HypixelAPI object
+    :param endpoint: The endpoint you want to fetch data from
+    :type endpoint: str
+    :param params: The parameters to send to the API
+    :type params: dict[str, str]
+    :param headers: The headers that are sent with the request
+    :type headers: dict[str, str]
+    :return: A dictionary containing the response from the Hypixel API.
+    """
+    HYPIXEL_API_URL = "https://api.hypixel.net"
 
+    async with ClientSession(HYPIXEL_API_URL, headers=headers) as session:
+        async with session.get(endpoint, params=params) as request:
+            ratelimit_info = {x: y for x, y in request.headers.items() if x.startswith("RateLimit-")}
             # ratelimit
+            print(request.status)
             if request.status == 429:
                 # add handling for ratelimiting
                 pass
             elif request.status == 200:
                 # check if it's successful
-                api.request_cache[f"{parsed.path.replace('/', '')}.{[param_value]}"] = {
-                    "response": request.json(),
+                response = await request.json()
+                hypixel_api.request_cache[f"{endpoint}.{params}"] = {
+                    "response": response,
                     "hypixel-api-python": {
                         "timestamp": int(datetime.now().timestamp())
                     }
                 }
-                return request.json()
+                hypixel_api.ratelimit_info = ratelimit_info
+                return response
             # add more status codes and else statement
