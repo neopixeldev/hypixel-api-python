720c1a491e99aa56e2f6613d0617dd0eb8d89413
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-07-10 20:37:14 -0400

    Add more support for caching

--- src/hypxiel_api_python/utils/requests.py
+++ src/hypxiel_api_python/utils/requests.py
@@ -1,34 +1,48 @@
+from src.hypxiel_api_python.utils.cache import purge_cache
+from urllib.parse import urlparse, parse_qs
 from aiohttp import ClientSession
 from datetime import datetime
 
 
 async def get(self, endpoint: str, param: tuple, nocache: bool = None):
-    api = self.hypixel_api
     """Determine whether it should use a cached object or fetch a new one."""
     """`nocache` is a way for the user to bypass the cache"""
-    cached_value = self.hypixel_api.endpoints[param][endpoint.replace("/", "")]
-    if not cached_value:
+    await purge_cache(self)
+    api = self.hypixel_api
+    cached_response = self.hypixel_api.request_cache[param][f"{endpoint.replace('/', '')}.{param[1]}"]
+    if not cached_response:
         return await fetch(self=self, url=f"{api.path}/{endpoint}?key={api.key}&{param[0]}={param[1]}")
 
-    elif cached_value["hypixel-api-python"]["timestamp"] + 60 > int(datetime.now().timestamp()) or nocache:
+    elif cached_response["hypixel-api-python"]["timestamp"] + 60 > int(datetime.now().timestamp()) or nocache:
         return await fetch(self=self, url=f"{api.path}/{endpoint}?key={api.key}&{param[0]}={param[1]}")
 
     else:
-        return cached_value
+        return cached_response["response"]
 
 
 async def fetch(self, url: str):
     """Fetches data from the Hypixel API"""
+    # stored as variables for readability
+    parsed = urlparse(url)
+    params = parse_qs(parsed.query)
+    param_name = list(params.keys())[1]
+    param_value = params[param_name][0]
+
     api = self.hypixel_api
     async with ClientSession() as session:
         async with session.get(url) as request:
 
-            # ratelimited
+            # ratelimit
             if request.status == 429:
                 # add handling for ratelimiting
                 pass
             elif request.status == 200:
-                # store in cache
                 # check if it's successful
+                api.request_cache[f"{parsed.path.replace('/', '')}.{[param_value]}"] = {
+                    "response": request.json(),
+                    "hypixel-api-python": {
+                        "timestamp": int(datetime.now().timestamp())
+                    }
+                }
                 return request.json()
             # add more status codes and else statement
